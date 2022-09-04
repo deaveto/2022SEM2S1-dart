9a9376943ac774c769a51a3cca644fdeecbea5b7
deaveto <53503206+deaveto@users.noreply.github.com>
2022-09-03 20:36:38 -0500

    Update excepciones.dart

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  try{
+    String elNombre = obtnerNombre("Armando");
+    print(elNombre);
+  } on FormatException catch(e){
+    print("se presento una excepción FormatException = $e");
+  } catch(e){
+    print("se presento una excepción general = $e");
+  }
+}
+String obtnerNombre(String nombre){
+  if(nombre == 'Diego'){
+    throw FormatException("Este nombre no esta permitido");
+  }
+    return 'Mi nombre es $nombre';
+}
