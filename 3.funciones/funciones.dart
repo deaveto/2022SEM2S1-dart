5d8978ae0039327e47ee5a9d29bfbe95dab1fc2a
deaveto <53503206+deaveto@users.noreply.github.com>
2022-09-03 20:34:42 -0500

    Update funciones.dart

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,20 @@
-void main() {}
+void main() {
+  print(obtenerNombre("Diego"));
+  print(obtenerNombreApellido("Diego", "Velasquez"));
+  print(obtenerNombreApellidoParameter(apellido: "Velasquez"));
+  print(miFuncionAnonima("armando"));
+}
+String obtenerNombre(String valor){
+  return 'Mi nombre es $valor';
+}
+String obtenerNombreApellido(String nombre, String apellido){
+  return 'Mi nombre es $nombre $apellido';
+}
+String obtenerNombreApellidoParameter({String? nombre, required String apellido, int edad = 0}){
+  return 'Mi nombre es $nombre $apellido, $edad';
+}
+// Funciones anonymous functions
+
+var miFuncionAnonima = (String valor){
+  return "el valor es $valor";
+};
