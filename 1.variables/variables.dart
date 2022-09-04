2f72645a994d8e373574576a7e1a1ed3a8ec73cd
deaveto <53503206+deaveto@users.noreply.github.com>
2022-09-03 20:33:11 -0500

    Update variables.dart

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  int variable1 =10;
+  double variable2 =1.4;
+
+  var variable3 =10;
+  var variable4 =1.4;
+
+
+  print(variable1);
+  print(variable2);
+  print(variable2.ceil()); //valor entero menor, pero mayor a el
+  print(variable2.floor()); //valor entero menor, pero menor a el
+  print(variable2.abs()); //valor absoluto
+  
+  
+  print(variable3);
+  print(variable4);
+}
